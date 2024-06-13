5b5eb8522018f8289e22b003c5f6bb3f85a0106d
kyungnankimKHT <165994108+kyungnankimKHT@users.noreply.github.com>
2024-06-13 10:22:00 +0900

    DTO 객체 th:each bolck

--- demoProject2/src/main/java/com/example/demo/controller/ExampleController.java
+++ demoProject2/src/main/java/com/example/demo/controller/ExampleController.java
@@ -1,9 +1,14 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.dto.StudentDTO;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
@@ -18,7 +23,8 @@ public class ExampleController {
 		 * - org.springframework.ui 패키지
 		 * - @SessionAttributes와 함께 사용할 경우 session scope 반환
 		 * [기본 사용법]
-		 * Model.addAttribute("key", value);
+		 * Model.addAttribute("key"               , value);
+		 * Model.addAttribute("html에 전달가능한 이름", 전달할 값을 작성);
 		 * */
 		@GetMapping("ex1") //  /example/ex1 주소로 보여지는 값
 		public String ex1(HttpServletRequest req, Model model) {
@@ -26,6 +32,60 @@ public class ExampleController {
 			// 나중에 DB에서 가져온 값을 보여줄 때 사용하는 메서드
 			req.setAttribute("test1", "HttpServletRequest로 전달한 값");
 			model.addAttribute("test2","Model로 전달한 값");
+			
+			// 단일 값(숫자, 문자열) Model을 이용해서 html 전달
+			model.addAttribute("productName", "종이컵");
+			
+			//price로 2000원을 전달
+			model.addAttribute("price",2000);
+			model.addAttribute("productCompany", "KHCOMPANY");
+			
+			// 복수 값(배열, List) Model을 이용해서 html 전달
+			List<String> fruitList = new ArrayList<>();
+			fruitList.add("사과");
+			fruitList.add("딸기");
+			fruitList.add("바나나");
+			
+			model.addAttribute("fruitList", fruitList);
+			
+			List<String> animal = new ArrayList<>();
+			animal.add("호랑이");
+			animal.add("토끼");
+			animal.add("거북이");
+			
+			model.addAttribute("animal", animal);
+			
+			
+			// DTO 객체 Model을 이용해서 html 전달
+			StudentDTO std = new StudentDTO();
+			std.setStudentNo("12345");
+			std.setName("홍길동");
+			std.setAge(30);
+			
+			model.addAttribute("std", std);
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
 			return "example/ex1"; // templates/example/ex1.html 파일 바라보는 것
 		}
 }
+
+
+
+
+
+
+
