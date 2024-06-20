13be87c7893e2382bfcd42aef012f5d51236ccb7
kyungnankimKHT <165994108+kyungnankimKHT@users.noreply.github.com>
2024-06-20 12:25:56 +0900

    join

---
+++ demoProject3-mybatis-mapper-select-image/src/main/java/com/khmall/dto/Pig.java
@@ -0,0 +1,15 @@
+package com.khmall.dto;
+
+import lombok.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@ToString
+public class Pig {
+	private int pig_id; //Number  int
+	private String pig_name; //Varchar2 String 
+	private int pig_age; //Number  int
+	private String pig_image_path; //Varchar2 String 
+}
---
+++ demoProject3-mybatis-mapper-select-image/src/main/java/com/khmall/mapper/PigMapper.java
@@ -0,0 +1,13 @@
+package com.khmall.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.khmall.dto.Pig;
+
+@Mapper
+public interface PigMapper {
+	//전체가져오기 -> List
+	List<Pig> getAllPigs();
+}
