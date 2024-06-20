4664c16b6f4b5bc6fe019ea9f8fc52471474bea8
kyungnankimKHT <165994108+kyungnankimKHT@users.noreply.github.com>
2024-06-20 14:14:08 +0900

    pig detail

--- demoProject3-mybatis-mapper-select-image/src/main/java/com/khmall/controller/PigController.java
+++ demoProject3-mybatis-mapper-select-image/src/main/java/com/khmall/controller/PigController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.khmall.dto.Pig;
 import com.khmall.service.PigService;
@@ -22,7 +23,12 @@ public class PigController {
 		return "index";
 	}
 	
-	@GetMapping()
+	@GetMapping("/pigDetail/{pig_id}")
+	public String getPigById(Model model, @PathVariable int pig_id) {
+		Pig pig = pigService.getPigById(pig_id);
+		model.addAttribute("pig", pig);
+		return "pigDetail";
+	}
 }
 
 
