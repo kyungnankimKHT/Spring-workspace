5d381f8f87b2fd518b5e6cbe2c16efa86cee3ba8
kyungnankimKHT <165994108+kyungnankimKHT@users.noreply.github.com>
2024-06-24 10:51:05 +0900

    pig_upload

--- demoProject3-mybatis-mapper-select-image/src/main/java/com/khmall/service/PigService.java
+++ demoProject3-mybatis-mapper-select-image/src/main/java/com/khmall/service/PigService.java
@@ -38,16 +38,17 @@ public class PigService {
 		//맨 끝에 /로 닫아주지 않으면 static/images/안에 이미지가 저장되는 것이 아니라
 		// static 바로 밑에 생성되기 때문에 이미지가 보이지 않으므로 반드시 마지막에 / 를 넣어줘야함
 		
-		String uploadDir = "C:/Users/user1/servlet_jsp_workspace/demoProject3-mybatis-mapper-select-image/src/main/resources/static/images/";
-
-		File imgFile = new File(uploadDir + fileName);
+		String uploadDir = "C:/Users/user1/Desktop/pig_img/";
+		
+		File imgFolder = new File(uploadDir + "cute/");
+		File imgFile = new File(imgFolder + fileName);
 		
 		// 나중에 이미지 파일을 저장하는 폴더가 존재하지 않을 경우 생성하는 코드
 		// 우리가 저장하고자 하는 파일 경로 설정
 		
-		if( !imgFile.exists()  /*만약에 이미지 폴더가 존재하지 않을 때가 true인 ! 추가*/) {
+		if( !imgFolder.exists()  /*만약에 이미지 폴더가 존재하지 않을 때가 true인 ! 추가*/) {
 			//폴더 생성하기
-			imgFile.mkdirs(); //폴더가 존재하지 않을 경우 우리가 지정한 폴더에 생성
+			imgFolder.mkdirs(); //폴더가 존재하지 않을 경우 우리가 지정한 폴더에 생성
 		}
 		
 		
@@ -59,7 +60,7 @@ public class PigService {
 			pig.setPig_age(pig_age);
 			//pig.setPig_image_path(uploadDir); //파일 이름만 저장
 			//만약에 파일경로와 이름을 같이 저장하길 원한다면 아래 코드를 사용
-			pig.setPig_image_path("/images/"+fileName);  //-> data에 /images/파일명.jpg
+			pig.setPig_image_path(imgFolder + "/"+fileName);  //-> data에 /images/파일명.jpg
 			 /******       uploadDir ->    fileName    변경해서    추가 *******/
 			pigMapper.uploadPig(pig);
 			System.out.println("파일 업로드 Service를 성공적으로 완료 했습니다.");
