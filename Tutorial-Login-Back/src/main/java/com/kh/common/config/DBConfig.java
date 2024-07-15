3628321fc42382657a97568d97ab2d6fb22c8f33
kyungnankimKHT <165994108+kyungnankimKHT@users.noreply.github.com>
2024-07-15 15:50:48 +0900

    config

--- Tutorial-Login-Back/src/main/java/com/kh/common/config/DBConfig.java
+++ Tutorial-Login-Back/src/main/java/com/kh/common/config/DBConfig.java
@@ -1,13 +1,19 @@
 package com.kh.common.config;
 
+import java.io.IOException;
+
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
@@ -21,7 +27,7 @@ import com.zaxxer.hikari.HikariDataSource;
 // 다른 properties 파일도 추가하고싶다면 어노테이션을 계속추가
 // @PropertySource("classpath:/config2.properties")
 // @PropertySource("classpath:/config3.properties")
-@PropertySource("classpath:/config.properties")
+@PropertySource("classpath:/config.properties") //****** 추후 변경 요망  ******//
 public class DBConfig {
 
 	@Autowired
@@ -46,6 +52,41 @@ public class DBConfig {
 	}
 	
 	////////// MyBatis 설정 추가 ////////// 
+	//SqlSessionFactory : SqlSession을 만드는 객체
+	@Bean
+	public SqlSessionFactory sessionFactory(DataSource dataSource) throws Exception {
+		SqlSessionFactoryBean sessionFactoryBean = new SqlSessionFactoryBean();
+		sessionFactoryBean.setDataSource(dataSource);
+		
+		// 매퍼 파일이 모여있는 경로 설정
+		sessionFactoryBean.setMapperLocations(applicationContext
+											.getResources("classpath:/mappers/**.xml")); //****** 추후 변경 요망  ******//
+		
+		// 별칭을 지정해야하는 DTO 가 모여있는 패키지 지정
+		// -> 해당 패키지에 있는 모든 클래스가 클래스명으로 별칭이 지정됨
+		// Aliase = 별칭 Aliases = 별칭들 Package = 폴더 밑에 있는 파일 모두
+		sessionFactoryBean.setTypeAliasesPackage("com.kh.dto"); //****** 추후 변경 요망  ******//
+		
+		// 마이바티스 설정 파일 경로 지정
+		sessionFactoryBean.setConfigLocation(applicationContext
+												.getResource("classpath:mybatis-config.xml")); //****** 추후 변경 요망  ******//
+		
+		return sessionFactoryBean.getObject();
+	}
+	
+	// 기본 SQL 실행 +  Commit 처리 = 트랜잭션 처리 commit  = 한글문서나 엑셀 최종 파일저장
+	// SqlSessionTemplate = 기본 sql 실행 + 트랜잭션 처리
+	@Bean
+	public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sessionFactory) {
+		return new SqlSessionTemplate(sessionFactory);
+	}
+	
+	// 전반적인 Commit 과 Rollback 과 같은 관리를 해주는 트랜잭션 매니저
+	// DataSourceTransactionManager = 트랜잭션 매니저
+	@Bean
+	public DataSourceTransactionManager dataSourceTransactionManager(DataSource dataSource) {
+		return new DataSourceTransactionManager(dataSource);
+	}
 }
 
 
---
+++ Tutorial-Login-Back/src/main/java/com/kh/mapper/TodoMapper.java
@@ -0,0 +1,12 @@
+package com.kh.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.kh.dto.TodoMember;
+
+@Mapper
+public interface TodoMapper {
+	int idCheck(String id);
+	
+	int signup(TodoMember member);
+}
